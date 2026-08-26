0970ffef7ab1b7d933bd29948d9da45106589c10
zqy <1090042044@qq.com>
2026-08-26 18:15:11 +0800

    proto协议工程

--- gen.bat
+++ gen.bat
@@ -4,5 +4,6 @@ set ROOT=%~dp0
 set ROOT=%ROOT:~0,-1%
 set GO_OUT=%ROOT%\..\grpc\pb\math
 if not exist "%GO_OUT%" mkdir "%GO_OUT%"
-protoc -I "%ROOT%" --go_out="%GO_OUT%" --go-grpc_out="%GO_OUT%" math.proto
+set PROTO_FILES=math.proto streamtest.proto types.proto
+protoc -I "%ROOT%" --go_out="%GO_OUT%" --go-grpc_out="%GO_OUT%" %PROTO_FILES%
 echo Go stubs generated into %GO_OUT%
--- gen.sh
+++ gen.sh
@@ -6,5 +6,6 @@ ROOT="$(cd "$(dirname "$0")" && pwd)"
 GO_OUT="$ROOT/../grpc/pb/math"
 
 mkdir -p "$GO_OUT"
-protoc -I "$ROOT" --go_out="$GO_OUT" --go-grpc_out="$GO_OUT" math.proto
+PROTO_FILES=(math.proto streamtest.proto types.proto)
+protoc -I "$ROOT" --go_out="$GO_OUT" --go-grpc_out="$GO_OUT" "${PROTO_FILES[@]}"
 echo "Go stubs generated into $GO_OUT"
